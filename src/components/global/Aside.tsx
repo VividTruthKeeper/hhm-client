05fe4900a3b8fb438b2ea58782a17a9fb9fc370c
Kakabay <kashyrberdyew@gmail.com>
2023-01-26 17:00:55 +0500

    aside animo

--- src/components/global/Aside.tsx
+++ src/components/global/Aside.tsx
@@ -1,10 +1,14 @@
 // Modules
 import { v4 as uuidv4 } from 'uuid';
+import { motion } from 'framer-motion';
 
 // Components
 import SectionTitle from './SectionTitle';
 import AsideItem from '../aside/AsideItem';
 import Calendar from '../aside/Calendar';
+
+// Icons
+import { ReactComponent as ArrowDownBlue } from '../../assets/icons/arrow-down-blue.svg';
 
 interface AsideDataType {
   title: string;
@@ -42,6 +46,37 @@ const asideData: AsideDataType[] = [
     category: 'Политика',
   },
 ];
+
+const buttonMotion = {
+  rest: {
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    color: '#2368ad',
+    duration: 0.4,
+    type: 'tween',
+  },
+  hover: {
+    backgroundColor: '#2368ad',
+    color: '#ffffff',
+    duration: 0.4,
+    type: 'spring',
+  },
+};
+
+const arrowMotion = {
+  rest: {
+    fill: '#2368ad',
+    transform: 'rotateX(0%)',
+    duration: 0.4,
+    type: 'tween',
+  },
+  hover: {
+    fill: '#ffffff',
+    transform: 'rotateX(180%)',
+    duration: 0.4,
+    type: 'spring',
+  },
+};
+
 const Aside = () => {
   return (
     <aside className="aside">
@@ -57,6 +92,16 @@ const Aside = () => {
           );
         })}
       </div>
+      <motion.button
+        className="aside-btn"
+        type="button"
+        initial={buttonMotion.rest}
+        whileHover={buttonMotion.hover}>
+        <span>Открыть полностью</span>
+        <motion.div initial={'rest'} variants={arrowMotion}>
+          <ArrowDownBlue />
+        </motion.div>
+      </motion.button>
       <Calendar />
     </aside>
   );
