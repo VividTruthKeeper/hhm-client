c13d26ac146093f511f78e332fb9fa8af8d75f7e
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-01-29 20:28:16 +0500

    subnav animation

--- src/animations/subNav.animations.ts
+++ src/animations/subNav.animations.ts
@@ -1,17 +1,20 @@
 export const linkMotion = {
   rest: {
+    fontWeight: "normal",
     background: "rgba(56,163,157, 0)",
     type: "tween",
   },
   active: {
+    fontWeight: "bold",
     background: "rgba(56,163,157, 1)",
     color: "#ffffff",
     type: "spring",
   },
-  linkRest: {
-    type: "tween",
-  },
-  linkHover: {
-    type: "spring",
-  },
+
+  // linkRest: {
+  //   type: "tween",
+  // },
+  // linkHover: {
+  //   type: "spring",
+  // },
 };
