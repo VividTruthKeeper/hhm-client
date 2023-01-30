14419b19d04b0a29d6d8fed452c8c53e524f0560
Kakabay <kashyrberdyew@gmail.com>
2023-01-30 20:20:43 +0500

    asside

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -2,47 +2,47 @@
 import SectionTitle from '../global/SectionTitle';
 import AsideNews from './AsideNews';
 // Images
-import placeholder from '../../assets/images/placeholder.jpg';
+import placeholder2 from '../../assets/images/placeholder2.png';
 
 const Aside = () => {
   return (
     <div className="aside">
       <div className="aside-wrapper">
-        <SectionTitle title="Лента новостей" linkData={{ link: '/', title: 'Посмотреть все' }} />
+        <SectionTitle title="Самое читаемое" />
         <div className="aside-inner">
           <AsideNews
             title="Президент Туркменистана провёл рабочее совещание по цифровой системе"
             date="12.01.2023"
             category="Политика"
-            img={placeholder}
+            img={placeholder2}
             link=""
           />
           <AsideNews
             title="Президент Туркменистана провёл рабочее совещание по цифровой системе"
             date="12.01.2023"
             category="Политика"
-            img={placeholder}
+            img={placeholder2}
             link=""
           />
           <AsideNews
             title="Президент Туркменистана провёл рабочее совещание по цифровой системе"
             date="12.01.2023"
             category="Политика"
-            img={placeholder}
+            img={placeholder2}
             link=""
           />
           <AsideNews
             title="Президент Туркменистана провёл рабочее совещание по цифровой системе"
             date="12.01.2023"
             category="Политика"
-            img={placeholder}
+            img={placeholder2}
             link=""
           />
           <AsideNews
             title="Президент Туркменистана провёл рабочее совещание по цифровой системе"
             date="12.01.2023"
             category="Политика"
-            img={placeholder}
+            img={placeholder2}
             link=""
           />
         </div>
