615744e6e63704e50251516af23b835ebff3efe2
Kakabay <kashyrberdyew@gmail.com>
2023-02-07 11:00:05 +0500

    videos component corrected

--- src/components/videos/Videos.tsx
+++ src/components/videos/Videos.tsx
@@ -1,18 +1,18 @@
 // Modules
-import { v4 as uuiv4 } from "uuid";
+import { v4 as uuiv4 } from 'uuid';
 
 // Components
-import SectionTitle from "../global/SectionTitle";
-import VideosItem from "./VideosItem";
+import SectionTitle from '../global/SectionTitle';
+import VideosItem from './VideosItem';
 
 // Images
-import Placeholder from "../../assets/images/placeholder.jpg";
+import Placeholder from '../../assets/images/placeholder.jpg';
 
 // Videos
-import VideoPlaceholder from "../../assets/videos/placeholder.mp4";
+import VideoPlaceholder from '../../assets/videos/placeholder.mp4';
 
 // Types
-import { videosDataType } from "../../types/videos.types";
+import { videosDataType } from '../../types/videos.types';
 
 const videosData: videosDataType[] = [
   {
@@ -36,26 +36,26 @@ const videosData: videosDataType[] = [
 const Videos = () => {
   return (
     <section className="videos">
-      <div className="container">
-        <div className="videos-inner">
-          <SectionTitle
-            title="Видео"
-            givenClass="videos"
-            linkData={{ link: "/", title: "Посмотреть все" }}
-          />
-          <div className="videos-items">
-            {videosData.map((videosDataItem: videosDataType) => {
-              return (
-                <VideosItem
-                  key={uuiv4()}
-                  url={videosDataItem.url}
-                  placeholder={videosDataItem.placeholder}
-                />
-              );
-            })}
-          </div>
+      {/* <div className="container"> */}
+      <div className="videos-inner">
+        <SectionTitle
+          title="Видео"
+          givenClass="videos"
+          linkData={{ link: '/', title: 'Посмотреть все' }}
+        />
+        <div className="videos-items">
+          {videosData.map((videosDataItem: videosDataType) => {
+            return (
+              <VideosItem
+                key={uuiv4()}
+                url={videosDataItem.url}
+                placeholder={videosDataItem.placeholder}
+              />
+            );
+          })}
         </div>
       </div>
+      {/* </div> */}
     </section>
   );
 };
