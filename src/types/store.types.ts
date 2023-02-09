db9cdc9afd3a519b25c6932920cf26c41ef13abd
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-10 01:20:38 +0500

    news scroll data type

--- src/types/store.types.ts
+++ src/types/store.types.ts
@@ -1,6 +1,9 @@
 // Reducers
 import { allReducers } from "../store/functionality";
 
+// Types
+import { IPostsData } from "./data.types";
+
 export interface ActiveLinkType {
   active: number;
 }
@@ -16,6 +19,14 @@ export interface ILanguage {
 export interface ILanguageAction {
   type: "SET_LANGUAGE";
   payload: "RU" | "EN" | "TM";
+}
+
+export interface INewsScroll {
+  data: IPostsData[];
+}
+export interface INewsScrollAction {
+  type: "SET_NEWS_SCROLL";
+  payload: INewsScroll["data"];
 }
 
 // ALL TYPES BEFORE THIS LINE ==================
