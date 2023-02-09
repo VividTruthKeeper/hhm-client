df9db820c98e8917e39a83c4d2abfd80270dcf8e
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-09 15:31:12 +0500

    commented

--- src/reducers/dataReducer.ts
+++ src/reducers/dataReducer.ts
@@ -1,14 +1,14 @@
-// Types
-import { ILanguage, ILanguageAction } from "../types/store.types";
+// // Types
+// import { ILanguage, ILanguageAction } from "../types/store.types";
 
-// Helpers
-import getSavedLanguage from "../helpers/getSavedLanguage";
+// // Helpers
+// import getSavedLanguage from "../helpers/getSavedLanguage";
 
-const initialState = {
-  title: getSavedLanguage(),
-};
+// const initialState = {
+//   title: getSavedLanguage(),
+// };
 
-export const languageReducer = (
-  state: ILanguage = initialState,
-  action: ILanguageAction
-) => {};
+// export const languageReducer = (
+//   state: ILanguage = initialState,
+//   action: ILanguageAction
+// ) => {};
