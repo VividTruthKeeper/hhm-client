0fad55ef988fd34a6b9a0f5aa89d716ff8d8e2fb
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-10 01:20:08 +0500

    news scroll params

---
+++ src/api/params.ts
@@ -0,0 +1,12 @@
+import { IurlParamAdder } from "../types/api.types";
+
+export const newsScrollParams: IurlParamAdder[] = [
+  {
+    name: "count",
+    value: 5,
+  },
+  {
+    name: "page",
+    value: 1,
+  },
+];
