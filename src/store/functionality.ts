f686a985b1f0608f10b4d1a1d8862f38a14c1132
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-10 01:20:29 +0500

    reducer added to all reducers

--- src/store/functionality.ts
+++ src/store/functionality.ts
@@ -4,10 +4,12 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 // Reducers
 import { activeLinkReducer } from "../reducers/activeLink.reducer";
 import { languageReducer } from "../reducers/language.reducer";
+import { dataReducer } from "../reducers/dataReducer";
 
 export const allReducers = combineReducers({
   activeLink: activeLinkReducer,
   language: languageReducer,
+  dataReducer,
 });
 
 export const functionalityStore = configureStore({
