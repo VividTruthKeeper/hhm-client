b019770c517543245106de4b8241ed5e69febe66
Kakabay <kashyrberdyew@gmail.com>
2023-02-28 16:07:33 +0500

    bg blur

--- src/components/main/ContentItem.tsx
+++ src/components/main/ContentItem.tsx
@@ -1,30 +1,32 @@
 // Modules
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import { Link } from "react-router-dom";
-import placeholder from "../../assets/images/placeholder.webp";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Link } from 'react-router-dom';
+import placeholder from '../../assets/images/placeholder.webp';
 
 interface IProps {
-  type?: "small" | "big";
+  type?: 'small' | 'big';
   img: string;
   title: string;
   id: number;
 }
 
-const ContentItem = ({ type = "small", img, title, id }: IProps) => {
+const ContentItem = ({ type = 'small', img, title, id }: IProps) => {
   return (
-    <div className={`main-content-item main-content-item__${type}`}>
+    <div
+      className={`main-content-item main-content-item__${type}`}
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}>
       <LazyLoadImage
         src={img}
         alt={img}
         effect="opacity"
         useIntersectionObserver
         placeholderSrc={placeholder}
-        style={{
-          backgroundImage: `url(${img})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
       />
+      <div className="bg-blur"></div>
       <Link to={`/news/${id}`} className="main-content-item-overlay">
         <h2>{title}</h2>
       </Link>
